4347953862a6074e8b0a31e544dd00c328076854
Mattan Dor <mattan.dor1@gmail.com>
2024-03-22 07:24:24 +0200

    Added: increase plugin version.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024032100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024032200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2018120310;        // Requires this Moodle version.
 $plugin->component = 'block_campaign_manager'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '1.0';
+$plugin->release = '1.1';
 $plugin->maturity = MATURITY_STABLE;
